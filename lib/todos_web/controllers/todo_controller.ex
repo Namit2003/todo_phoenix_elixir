65a409d7eb6ccf6c0b0613526f0856347e9011e7
Namit <namit.k.patel@gmail.com>
2024-12-11 18:20:51 +0530

    update and delete done

--- lib/todos_web/controllers/todo_controller.ex
+++ lib/todos_web/controllers/todo_controller.ex
@@ -25,4 +25,20 @@ defmodule TodosWeb.TodoController do
       render(conn, :show, todo: todo)
     end
   end
+
+  def update(conn, %{"id" => id, "todo" => todo_params}) do
+    todo = Todolist.get_todo!(id)
+
+    with {:ok, %Todolist.Todo{} = todo} <- Todolist.update_todo(todo, todo_params) do
+      render(conn, :show, todo: todo)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    todo = Todolist.get_todo!(id)
+
+    with {:ok, %Todolist.Todo{}} <- Todolist.delete_todo(todo) do
+      send_resp(conn, :no_content, "")
+    end
+  end
 end
